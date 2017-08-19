88f8e5073b580f3200cd44d5e82c12c9b568ca09
caleblareau <caleblareau@g.harvard.edu>
2017-08-19 17:39:25 -0400

    better documentation with one / gather

--- Pypkg/mgatk/tests/usingOneExample.sh
+++ Pypkg/mgatk/tests/usingOneExample.sh
@@ -1,7 +1,15 @@
+#!/bin/bash
 
+# Standard usage
+mgatk call -i humanbam -o out -n glio
 
-mgatk one -i tests/humanbam/MGH60-P6-A11.mito.bam -o oneout
-mgatk one -i tests/humanbam/MGH60-P6-B01.mito.bam -o oneout
+# This should give the same output as what's above
+# expect for some minor file names (output directory / base name)
+
+mgatk one -i humanbam/MGH60-P6-A11.mito.bam -o oneout
+mgatk one -i humanbam/MGH60-P6-B01.mito.bam -o oneout
+mgatk one -i humanbam/MGH97-P8-H02.mito.bam -o oneout
+mgatk one -i humanbam/MGH97-P8-H03.mito.bam -o oneout
 
 mgatk gather -i oneout -n glioOne
 
