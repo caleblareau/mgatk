c7940341e073b5048d4e27d032a78416ee7a74c0
Anton Larsson <antonlarsson23@gmail.com>
2022-05-11 16:29:24 +0200

    speed up split_barcoded_bam

--- mgatk/bin/python/split_barcoded_bam.py
+++ mgatk/bin/python/split_barcoded_bam.py
@@ -16,35 +16,35 @@ mtchr = sys.argv[5]
 base=os.path.basename(bamfile)
 basename=os.path.splitext(base)[0]
 
-def getBarcode(intags):
+def getBarcode(read):
 	'''
 	Parse out the barcode per-read
 	'''
-	for tg in intags:
-		if(barcodeTag == tg[0]):
-			return(tg[1])
-	return("NA")
+	if read.has_tag(barcodeTag):
+		return read.get_tag(barcodeTag)
+	else:
+		return "NA"
 
 
-def writePassingReads(bc, mtchr):
+def writePassingReads(bc_dict, mtchr):
 	'''
 	Write out reads to their corresponding files based on a barcode index
 	'''
-	bam = pysam.AlignmentFile(bamfile,'rb')
+	bam = pysam.AlignmentFile(bamfile,'rb', threads=5)
 	Itr = bam.fetch(str(mtchr),multiple_iterators=False)
 	for read in Itr:
-		read_barcode = getBarcode(read.tags)
+		read_barcode = getBarcode(read)
 		
 		# If read barcode is in whitelist, then write it out
-		if read_barcode in bc:
-			idx = bc.index(read_barcode)
+		if read_barcode in bc_dict:
+			idx = bc_dict[read_barcode]
 			file = fopen[idx]
 			file.write(read)
 
 # Read in the barcodes
 with open(bcfile) as barcode_file_handle:
     content = barcode_file_handle.readlines()
-bc = [x.strip() for x in content] 
+bc = [x.strip() for x in content]
 
 # Open up a bunch of files and write out reads for valid barcodes
 @contextmanager
@@ -61,8 +61,9 @@ def multi_file_manager(files, mode='rt'):
 		
 # Final loop to write out passing reads
 bambcfiles = [outfolder + "/" + bc1 + ".bam" for bc1 in bc]
+bc_dict = {bc1: i  for i,bc1 in enumerate(bc)}
 with multi_file_manager(bambcfiles) as fopen:
-	writePassingReads(bc, mtchr)
+	writePassingReads(bc_dict, mtchr)
 
 
 
