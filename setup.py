e8bc4e47d91224ced7aa6e87c08b5568945a60c7
caleblareau <clareau@stanford.edu>
2023-06-26 14:28:05 -0700

    v0.6.8 bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ dependencies = ['click', 'pysam', 'pytest', 'snakemake', 'biopython', 'numpy', '
 
 setup(
     name='mgatk',
-    version='0.6.7',
+    version='0.6.8',
     url='https://github.com/caleblareau/mgatk',
     license='MIT',
     author='Caleb Lareau',
